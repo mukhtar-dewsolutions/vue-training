ae04021d26d2e1b88d5ae29b69257e3a38ba897d
mukhtar-dewsolutions <mukhtara@dewsolutions.in>
2023-11-02 06:04:50 +0530

    added project setup with login html css code

---
+++ login-task/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Login />
+    <Child v-if="false" msg="Welcome to my 'Vue.js + TypeScript' app" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Child from "./components/Child.vue";
+import Login from "./components/Login.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Login,
+    Child,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
